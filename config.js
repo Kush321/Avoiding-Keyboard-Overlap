664f174b9e366397b1e9cb7266b51c4de58166f5
Kush321 <78313670+Kush321@users.noreply.github.com>
2021-07-11 12:07:52 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+require('@firebase/firestore');
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCg69N5-e4LW_ObVLKDhelizSyqJpNClQk",
+  authDomain: "wily-8b487.firebaseapp.com",
+  databaseURL: "https://wily-app-8b487.firebaseio.com", 
+  projectId: "wily-8b487",
+  storageBucket: "wily-8b487.appspot.com",
+  messagingSenderId: "237181899005",
+  appId: "1:237181899005:web:575129665d95bc7effae94"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
